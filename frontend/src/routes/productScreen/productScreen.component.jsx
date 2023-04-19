30349241561491c9bd345bf7800f231ade70e2c2
Crpearce <Crpearce892@gmail.com>
2023-04-19 10:48:47 -0600

    update product screen view with match

--- frontend/src/routes/productScreen/productScreen.component.jsx
+++ frontend/src/routes/productScreen/productScreen.component.jsx
@@ -1,9 +1,12 @@
-const ProductScreen = () => {
-    return (
-        <div>
-            Product Screen
-        </div>
-    );
+import { Link, useParams } from 'react-router-dom'
+import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap'
+import Rating from '../../components/rating/rating.component'
+import products from '../../products'
+
+const ProductScreen = ({ match }) => {
+  const params = useParams()
+  const product = products.find((p) => p._id === params.id)
+  return <div>{product.name}</div>
 }
 
-export default ProductScreen;
+export default ProductScreen
