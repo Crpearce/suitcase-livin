445ea026144b654cd28326d5025e170adf7abafa
Crpearce <Crpearce892@gmail.com>
2023-04-20 09:53:16 -0600

    fetch single product data

--- frontend/src/routes/productScreen/productScreen.component.jsx
+++ frontend/src/routes/productScreen/productScreen.component.jsx
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from 'react'
 import { Link, useParams } from 'react-router-dom'
 import {
   Row,
@@ -9,11 +10,20 @@ import {
   Button,
 } from 'react-bootstrap'
 import Rating from '../../components/rating/rating.component'
-import products from '../../products'
+import axios from 'axios'
 
-const ProductScreen = ({ match }) => {
+const ProductScreen = () => {
+  const [product, setProduct] = useState({})
   const params = useParams()
-  const product = products.find((p) => p._id === params.id)
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const { data } = await axios.get(`/api/products/${params.id}`)
+      setProduct(data)
+    }
+    fetchProduct()
+  }, [params.id])
+
   return (
     <>
       <Link className='btn btn-light my-3' to='/'>
