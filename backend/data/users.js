322da46f018cd6f79c31529ac9ac520d2bdcf237
Crpearce <Crpearce892@gmail.com>
2023-04-24 09:10:47 -0600

    add bcrypt and user data

---
+++ backend/data/users.js
@@ -0,0 +1,17 @@
+import bcrypt from 'bcryptjs'
+
+const users = [
+  {
+    name: 'Darik Angeli',
+    email: 'crpearce892@gmail.com',
+    password: bcrypt.hashSync('123456', 10),
+    idAdmin: true,
+  },
+  {
+    name: 'Colby Pearce',
+    email: 'cpearce22@turing.edu',
+    password: bcrypt.hashSync('123456', 10),
+  },
+]
+
+export default users
