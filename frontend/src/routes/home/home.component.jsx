eb075ec529524a7117bff4a67594995f6e78776a
Crpearce <Crpearce892@gmail.com>
2023-04-25 14:43:11 -0600

    swap axios call to home screen with redux

--- frontend/src/routes/home/home.component.jsx
+++ frontend/src/routes/home/home.component.jsx
@@ -1,29 +1,43 @@
-import React, { useState, useEffect } from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { Col, Row } from 'react-bootstrap'
 import Product from '../../components/product/product.component'
-import axios from 'axios'
+import { listProducts } from '../../actions/productActions'
 
 const Home = () => {
-  const [products, setProducts] = useState([])
+  const dispatch = useDispatch()
+  // productList should be the same name as it is listed in your store
+  const productList = useSelector((state) => state.productList)
+  // destructure and take the pieces we need
+  const { loading, products, error } = productList
+
+  // by dispatching listProducts we are dispatching PRODUCT_LIST_REQUEST in our actions
+  // it was successful, so we pass the new data into the payload, finally with the help of
+  // the productListReducer- our action.payload is being put into the products array
+  // on a sucessful response
 
   useEffect(() => {
-    const fetchProducts = async () => {
-      const { data } = await axios.get('/api/products')
-      setProducts(data)
-    }
-    fetchProducts()
-  }, [])
+    dispatch(listProducts())
+  }, [dispatch])
+
+  // to actual display the data from redux we will need to use useSelector
 
   return (
     <>
       <h1>latest Products</h1>
-      <Row>
-        {products.map((product) => (
-          <Col sm={12} md={6} lg={4} xl={3} key={product._id}>
-            <Product product={product} />
-          </Col>
-        ))}
-      </Row>
+      {loading ? (
+        <h2>Loading...</h2>
+      ) : error ? (
+        <h3>{error}</h3>
+      ) : (
+        <Row>
+          {products.map((product) => (
+            <Col sm={12} md={6} lg={4} xl={3} key={product._id}>
+              <Product product={product} />
+            </Col>
+          ))}
+        </Row>
+      )}
     </>
   )
 }
